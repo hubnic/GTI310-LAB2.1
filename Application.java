7b8f094abdbf2ea7e4376c2c1dc4a169cf2c0c2e
Nicolas <nicolas.hubert.1@etsmtl.net>
2015-01-29 14:06:00 -0500

    CREATION

---
+++ Application.java
@@ -0,0 +1,12 @@
+
+
+public class Application {
+
+	/**
+	 * Launch the application
+	 * @param args This parameter is ignored
+	 */
+	public static void main(String args[]) {
+		System.out.println("Audio Resample project!");
+	}
+}
---
+++ audio/ConcreteAudioFilter.java
@@ -0,0 +1,19 @@
+package audio;
+
+public class ConcreteAudioFilter implements AudioFilter{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void process() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
